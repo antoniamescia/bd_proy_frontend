c20450b90fe5d68d211d56e7a98fa78b81829f0e
antoniamescia <antonia.mescia.novarese@gmail.com>
2022-11-09 23:33:06 -0300

    Inital commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'bd_proyecto_front';
+}
