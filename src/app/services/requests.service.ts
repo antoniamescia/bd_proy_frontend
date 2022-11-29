fba3d8181426aa164a5955f84c8de0e535b94b21
antoniamescia <antonia.mescia.novarese@gmail.com>
2022-11-29 03:09:19 -0300

    3am

---
+++ src/app/services/requests.service.ts
@@ -0,0 +1,22 @@
+import { AnimateTimings } from '@angular/animations';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestsService {
+
+  constructor(private http: HttpClient) { }
+
+  url = 'https://bd-api.nicocartalla.com/api/v1/';
+
+  getRequests() {
+    return this.http.get(this.url+'permissions/requests');
+  }
+
+  handleRequest(estado: any, userId: any, appId: any, rolNegId: any) {
+    return this.http.put(this.url+'permissions/requests', {estado, userId, appId, rolNegId}, {observe: 'response'});
+  }
+}
